6c03f06803ca4f1162fcc273be10339ab0e66595
sunilgiri7 <seungiri841@gmail.com>
2023-12-30 00:46:40 -0800

    'New'

--- studygate/settings.py
+++ studygate/settings.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = [
     BASE_DIR/ 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
